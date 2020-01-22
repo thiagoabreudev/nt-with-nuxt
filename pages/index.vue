ba3f2251382fa1cd8f585afb6abe91f69c73b378
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2020-01-21 23:47:25 -0300

    created test blog

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nt-with-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My peachy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <nuxt-link to="/blog">Blog</nuxt-link>
   </div>
 </template>
 
